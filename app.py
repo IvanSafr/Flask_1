95dc031a982e2977846968711fad67dcdbfd5af4
ivans <ivansafronov997@gmail.com>
2023-03-11 19:20:13 +0300

    Database_1

--- app.py
+++ app.py
@@ -1,6 +1,22 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime as dt
 
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///blog.db"
+db = SQLAlchemy(app)
+
+class DatB(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(50), nullable=False)
+    intro = db.Column(db.String(200), nullable=False)
+    text = db.Column(db.Text, nullable=False)
+    date = db.Column(db.DateTime, default=dt.utsnow)
+
+    def __repair__(self):
+        return '<DatB %r' %self.id
+
 
 @app.route('/')
 @app.route('/home')
