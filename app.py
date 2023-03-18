2e2dadf4b0243144e16d7f712f9288852778169f
ivans <ivansafronov997@gmail.com>
2023-03-18 17:01:44 +0300

    Database_2

--- app.py
+++ app.py
@@ -1,27 +1,53 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime as dt
 
 app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///blog.db"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
+app.app_context().push()
 
 class DatB(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(50), nullable=False)
     intro = db.Column(db.String(200), nullable=False)
     text = db.Column(db.Text, nullable=False)
-    date = db.Column(db.DateTime, default=dt.utsnow)
+    date = db.Column(db.DateTime, default=dt.utcnow)
 
     def __repair__(self):
         return '<DatB %r' %self.id
 
+@app.route('/library')
+def posts_page():
+    posts = DatB.query.order_by(DatB.date).all()
+
+    return render_template("posts.html", posts=posts)
 
 @app.route('/')
 @app.route('/home')
 def index():
     return render_template("index.html")
+
+@app.route('/create', methods=['POST', 'GET'])
+def create():
+    if request.method == "POST":
+        title = request.form['title']
+        intro = request.form['intro']
+        text = request.form['text']
+
+        art = DatB(title=title, intro=intro, text=text)
+        try:
+            db.session.add(art)
+            db.session.commit()
+            return redirect('/')
+        except:
+            return "Еклмн, ошибка"
+
+
+    else:
+        return render_template("create.html")
 
 @app.route('/us')
 def us():
