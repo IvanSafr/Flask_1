ed7d1cf389b8283e4f42d985290c601dfba6185b
ivans <ivansafronov997@gmail.com>
2023-03-21 08:25:05 +0300

    Final_build

--- app.py
+++ app.py
@@ -19,11 +19,49 @@ class DatB(db.Model):
     def __repair__(self):
         return '<DatB %r' %self.id
 
+
 @app.route('/library')
 def posts_page():
-    posts = DatB.query.order_by(DatB.date).all()
+    posts = DatB.query.order_by(DatB.date.desc()).all()
 
     return render_template("posts.html", posts=posts)
+
+@app.route('/posts/<int:id>')
+def posts_detail(id):
+    post = DatB.query.get(id)
+
+    return render_template("post.html", post=post)
+
+
+@app.route('/posts/<int:id>/delete')
+def posts_del(id):
+    post = DatB.query.get_or_404(id)
+
+    try:
+        db.session.delete(post)
+        db.session.commit()
+        return redirect("/library")
+    except:
+        return "Ошибка удаления"
+
+@app.route('/posts/<int:id>/update', methods=['POST', 'GET'])
+def update(id):
+    post = DatB.query.get(id)
+    if request.method == "POST":
+        post.title = request.form['title']
+        post.intro = request.form['intro']
+        post.text = request.form['text']
+
+        art = DatB(title=post.title, intro=post.intro, text=post.text)
+        try:
+            db.session.add(art)
+            db.session.commit()
+            return redirect('/library')
+        except:
+            return "Еклмн, ошибка"
+    else:
+        post = DatB.query.get(id)
+        return render_template("update.html", post=post)
 
 @app.route('/')
 @app.route('/home')
@@ -41,7 +79,7 @@ def create():
         try:
             db.session.add(art)
             db.session.commit()
-            return redirect('/')
+            return redirect('/library')
         except:
             return "Еклмн, ошибка"
 
